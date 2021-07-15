f89c04c936a5c1a5fc7bfb6160fe1399cc5d1e3b
RozaliaLI <roilatypova@gmail.com>
2021-07-15 22:54:57 +0300

    Added functions

--- sandbox/src/main/java/roli/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/roli/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,25 @@ package roli.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, Roza!");
+    hello("world");
+
+    double k = 5;
+    System.out.println("Площадь квадрата со стороной " + k + " = " + area(k));
+
+    double a = 1;
+    double b = 2;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b) );
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
