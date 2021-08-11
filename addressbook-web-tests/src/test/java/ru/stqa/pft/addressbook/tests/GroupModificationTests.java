a6e217b6086824aa089f6830cde35cd37d75741b
RozaliaLI <roilatypova@gmail.com>
2021-08-11 18:03:00 +0300

    Create GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  public void testGroupModification() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("Test2", "Test 2", "Test 3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
